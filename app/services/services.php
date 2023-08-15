1920861f63bcf7282bc6f4f839e15ea8192ce87c
Kamaal22 <Kamaalkcy@gmail.com>
2023-08-15 17:54:37 +0300

    added the `change password` file

--- app/services/services.php
+++ app/services/services.php
@@ -65,7 +65,8 @@ include_once('./app/database/conn.php')
 
 
 <!-- Modal -->
-<div class="modal fade" id="serviceModal" tabindex="-1" role="dialog" aria-labelledby="serviceModalLabel" aria-hidden="true">
+<div class="modal fade" id="serviceModal" tabindex="-1" role="dialog" aria-labelledby="serviceModalLabel"
+    aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -80,18 +81,22 @@ include_once('./app/database/conn.php')
                     <input type="hidden" name="id" id="id">
                     <div class="mb-3">
                         <label for="name" class="form-label text-primary ">Service Name</label>
-                        <input type="text" class="form-control border border-1 border-primary" id="name" name="name" required>
+                        <input type="text" class="form-control border border-1 border-primary" id="name" name="name"
+                            required>
                     </div>
                     <div class="mb-3">
                         <label for="description" class="form-label text-primary">Description</label>
-                        <textarea class="form-control border border-1 border-primary" id="description" name="description" required></textarea>
+                        <textarea class="form-control border border-1 border-primary" id="description"
+                            name="description" required></textarea>
                     </div>
                     <div class="mb-3">
                         <label for="fee" class="form-label text-primary">Fee</label>
-                        <input type="number" class="form-control border border-1 border-primary" id="fee" name="fee" required>
+                        <input type="number" class="form-control border border-1 border-primary" id="fee" name="fee"
+                            required>
                     </div>
                     <div class="text-center">
-                        <button type="Button" class="btn btn-outline-primary" id="submit" onclick="insertUpdate()">Submit</button>
+                        <button type="Button" class="btn btn-outline-primary" id="submit"
+                            onclick="insertUpdate()">Submit</button>
                     </div>
                 </form>
             </div>
@@ -105,95 +110,95 @@ include_once('./app/database/conn.php')
 
 </html>
 <script>
-    function editServices(ids) {
+function editServices(ids) {
 
-        var id = ids;
-        $('#id').val(id);
-        //get services data
+    var id = ids;
+    $('#id').val(id);
+    //get services data
 
-        $.ajax({
-            url: './app/services/getService.php',
-            type: 'POST',
-            data: {
-                updateid: id
-            },
-            success: function(response) {
-                var data = JSON.parse(response);
-                // alert(data.name);
-                $('#name').val(data.name);
-                $('#description').val(data.description);
-                $('#fee').val(data.fee);
-                // show modal
-                $('#serviceModal').modal('show');
-            }
-
-        });
-        $("#submit").text('Update');
-    }
-
-    function deleteServices(id) {
-        var id = id;
-        $.ajax({
-            url: './app/services/deleteService.php',
-            type: 'POST',
-            data: {
-                deleteid: id
-            },
-            success: function(response) {
-                var obj = jQuery.parseJSON(response);
-                if (obj.status == 200) {
-                    location.reload();
-                } else {
-                    alert(obj.message);
-                }
-            }
-        });
-    }
-
-    $(document).ready(function() {
-        $('#dataTable').DataTable({
-            "pagingType": "full_numbers",
-            "lengthMenu": [
-                [10, 25, 50, -1],
-                [10, 25, 50, "All"]
-            ],
-            responsive: true,
-            language: {
-                search: "_INPUT_",
-                searchPlaceholder: "Search records",
-            }
-        });
-        //$('#dataTable').DataTable();
-
+    $.ajax({
+        url: './app/services/getService.php',
+        type: 'POST',
+        data: {
+            updateid: id
+        },
+        success: function(response) {
+            var data = JSON.parse(response);
+            // alert(data.name);
+            $('#name').val(data.name);
+            $('#description').val(data.description);
+            $('#fee').val(data.fee);
+            // show modal
+            $('#serviceModal').modal('show');
+        }
 
     });
+    $("#submit").text('Update');
+}
 
-    function insertUpdate() {
-
-        var name = $('#name').val();
-        var description = $('#description').val();
-        var fee = $('#fee').val();
-        var id = $('#id').val();
-        var ulr = './app/services/process_service.php';
-        var data = {
-            name: name,
-            description: description,
-            fee: fee,
-            id: id
-        };
-
-        $.ajax({
-            url: ulr,
-            type: 'POST',
-            data: data,
-            success: function(response) {
-                var obj = jQuery.parseJSON(response);
-                if (obj.status == 200) {
-                    location.reload();
-                } else {
-                    alert(obj.message);
-                }
+function deleteServices(id) {
+    var id = id;
+    $.ajax({
+        url: './app/services/deleteService.php',
+        type: 'POST',
+        data: {
+            deleteid: id
+        },
+        success: function(response) {
+            var obj = jQuery.parseJSON(response);
+            if (obj.status == 200) {
+                location.reload();
+            } else {
+                alert(obj.message);
             }
-        });
-    }
+        }
+    });
+}
+
+$(document).ready(function() {
+    $('#dataTable').DataTable({
+        "pagingType": "full_numbers",
+        "lengthMenu": [
+            [10, 25, 50, -1],
+            [10, 25, 50, "All"]
+        ],
+        responsive: true,
+        language: {
+            search: "_INPUT_",
+            searchPlaceholder: "Search records",
+        }
+    });
+    //$('#dataTable').DataTable();
+
+
+});
+
+function insertUpdate() {
+
+    var name = $('#name').val();
+    var description = $('#description').val();
+    var fee = $('#fee').val();
+    var id = $('#id').val();
+    var ulr = './app/services/process_service.php';
+    var data = {
+        name: name,
+        description: description,
+        fee: fee,
+        id: id
+    };
+
+    $.ajax({
+        url: ulr,
+        type: 'POST',
+        data: data,
+        success: function(response) {
+            var obj = jQuery.parseJSON(response);
+            if (obj.status == 200) {
+                location.reload();
+            } else {
+                alert(obj.message);
+            }
+        }
+    });
+}
 </script>
